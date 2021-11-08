d3739e920c1c2dc6db0cee8ecdbc74e1de31d221
izumi32 <k.izumi721@gmail.com>
2021-11-08 19:27:30 +0900

    ch10-2

--- lib/prac.rb
+++ lib/prac.rb
@@ -1,9 +1,41 @@
-require 'date'
-
-def to_date(string)
-  raise
-    Date.parse string rescue nil
+def greeting(&block)
+  puts "こんにちは"
+  unless block.nil?
+    text = block.call("おはよう")
+    puts text
+  end
+  puts "こんばんわ"
 end
 
-p to_date("2019-11-11")
-p to_date("aksak")
+# greeting
+# # greeting do
+# #   puts "おはよう"
+# # end
+#
+# greeting do |text|
+#   text * 2
+# end
+
+def greeting_ja(&block)
+  texts = ["おはよう", "こんにちは", "こんばんわ"]
+  greeting_common(texts, &block)
+end
+
+def greeting_en(&block)
+  texts = ["good morning", "hello", "good evening"]
+  greeting_common(texts, &block)
+end
+
+def greeting_common(texts, &block)
+  puts block.call(texts[0])
+end
+
+greeting_ja do |text|
+  text * 2
+end
+
+greeting_en do |text|
+  text.upcase
+end
+
+greeting_en { |text| text * 2 }

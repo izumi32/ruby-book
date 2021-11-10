59273fcffd5ec5b49c366306771cc5d2d0911d61
izumi32 <k.izumi721@gmail.com>
2021-11-10 21:40:18 +0900

    ch11-4

--- lib/prac.rb
+++ lib/prac.rb
@@ -1,11 +1,3 @@
-def greeting(proc_1, proc_2, proc_3)
-  puts proc_1.call("おは")
-  puts proc_2.call("こんに")
-  puts proc_3.call("こんば")
-end
-
-shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
-repeat_proc = Proc.new { |text| text * 2 }
-question_proc = Proc.new { |text| "#{text}?" }
-
-greeting(shuffle_proc, repeat_proc, question_proc)
+a = "hello ".tap { |s| puts "<s>"}
+a
+p a

415ed6caa0f6aadcfb746f805e05ed61393f9cb0
izumi32 <k.izumi721@gmail.com>
2021-11-11 23:33:23 +0900

    ch12-4

--- lib/prac.rb
+++ lib/prac.rb
@@ -1,3 +1,16 @@
-a = "hello ".tap { |s| puts "<s>"}
-a
-p a
+require 'yaml'
+
+yaml = <<TEXT
+alice:
+  name: "Alice"
+  email: "alice@gmail.com"
+  age: 20
+TEXT
+
+users = YAML.load(yaml)
+p users["alice"]
+p users["alice"]["name"]
+
+users["alice"]["gender"] = :female
+
+p YAML.dump(users)

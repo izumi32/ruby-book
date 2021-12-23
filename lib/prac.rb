40fbb278a3af3b5949a32ce9f7fcdfba24f1019e
izumi32 <k.izumi721@gmail.com>
2021-12-23 22:09:43 +0900

    ch7-5

--- lib/prac.rb
+++ lib/prac.rb
@@ -1,25 +1,16 @@
 class User
-  attr_reader :first_name
-  attr_reader :last_name
-  attr_reader :age
 
-  def initialize(first_name, last_name, age)
-    @first_name = first_name
-    @last_name = last_name
-    @age = age
+  puts "クラス直下#{self}"
+
+  def foo
+    puts "インスタンスメソッド#{self}"
   end
 
-  def full_name
-    "#{self.first_name}#{self.last_name}"
+  def self.bar
+    puts "クラスメソッド#{self}"
   end
-
-  def hello
-    "Hello, #{first_name}"
-  end
-
 end
 
-user = User.new('izumi', 'kento', '28')
-p user.first_name
-p user.full_name
-p user.hello
+user = User.new
+user.foo
+User.bar

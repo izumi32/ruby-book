2ec74d9a90458e5980c7b74d7eb0eb7136d864dd
izumi32 <k.izumi721@gmail.com>
2021-12-24 00:48:09 +0900

    7-6

--- lib/prac.rb
+++ lib/prac.rb
@@ -1,16 +1,30 @@
-class User
+class Product
+  attr_reader :name, :price
 
-  puts "クラス直下#{self}"
-
-  def foo
-    puts "インスタンスメソッド#{self}"
+  def initialize(name, price)
+    @name = name
+    @price = price
   end
 
-  def self.bar
-    puts "クラスメソッド#{self}"
+  def to_s
+    "#{name}, #{price}"
   end
 end
 
-user = User.new
-user.foo
-User.bar
+class DVD < Product
+  attr_reader :running_time
+
+  def initialize(name, price, running_time)
+    super(name, price)
+    @running_time = running_time
+  end
+
+  def to_s
+    super + ", #{running_time}"
+  end
+end
+
+product = Product.new('drama', 800)
+dvd = DVD.new('drama', 800, 60)
+p product.to_s
+p dvd.to_s

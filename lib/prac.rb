2541da0528f93552c17a0996abad31a79783b424
izumi32 <k.izumi721@gmail.com>
2022-02-03 16:25:12 +0900

    test

--- lib/prac.rb
+++ lib/prac.rb
@@ -1,12 +1,23 @@
-class User
-  class BloodType
-    attr_reader :type
+module Loggable
 
-    def initialize(type)
-      @type = type
-    end
+  private
+
+  def log(text)
+    puts "LOG #{text}"
   end
 end
 
-blood_type = User::BloodType.new('A')
-blood_type.type
+class Product
+
+  include Loggable
+
+  def title
+    log 'title is called'
+    'A great muvie'
+  end
+end
+
+product = Product.new
+product.title
+
+test_git ='test_git'
